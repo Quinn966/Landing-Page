9517adad42b79c0360f77fd8487860d253c0d5a2
Marcos Vinícius <marcosvinicius.as96@gmail.com>
2022-03-08 14:10:35 -0300

    landing page

---
+++ src/App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+import './App.css';
+import Main from './Main'
+import Conteudo from './Conteudo'
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+      <Conteudo />
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Conteudo.js
@@ -0,0 +1,27 @@
+function Conteudo() {
+    return (
+        <div className='Conteudo'>
+            <div className='center'>
+                <div className='conteudo-single'>
+                    <h3>Titulo</h3>
+                    <p>Isso é somente para questões de estudo e aperfeiçoamento das minhas tecnicas em programação</p>
+                </div>
+                <div className='conteudo-single'>
+                    <h3>Titulo</h3>
+                    <p>Isso é somente para questões de estudo e aperfeiçoamento das minhas tecnicas em programação</p>
+                </div>
+                <div className='conteudo-single'>
+                    <h3>Titulo</h3>
+                    <p>Isso é somente para questões de estudo e aperfeiçoamento das minhas tecnicas em programação</p>
+                </div>
+                <div className='conteudo-single'>
+                    <h3>Titulo</h3>
+                    <p>Isso é somente para questões de estudo e aperfeiçoamento das minhas tecnicas em programação</p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Conteudo
