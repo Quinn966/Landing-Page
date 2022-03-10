706c4e5140ffcc61e891e38f8fae514c46fcfa20
Marcos Vinícius <marcosvinicius.as96@gmail.com>
2022-03-10 11:52:15 -0300

    att1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Main from './Main'
 import Conteudo from './Conteudo'
 
+
 function App() {
   return (
     <div className="App">
--- src/Conteudo.js
+++ src/Conteudo.js
@@ -3,20 +3,22 @@ function Conteudo() {
         <div className='Conteudo'>
             <div className='center'>
                 <div className='conteudo-single'>
-                    <h3>Titulo</h3>
-                    <p>Isso é somente para questões de estudo e aperfeiçoamento das minhas tecnicas em programação</p>
+                    <h3>Quem sou</h3>
+                    <p>Me chamo Marcos Vinícius de Assis Gomes, Desenvolvedor front-end autodidata focado em escrever código limpo, eficiente e legível.
+Em todas minhas experiencias passadas me destaquei por apresentar liderança e dedicação, hoje busco um novo desafio em estudando e me aperfeiçoando todos os dias na área de programação.</p>
                 </div>
                 <div className='conteudo-single'>
-                    <h3>Titulo</h3>
-                    <p>Isso é somente para questões de estudo e aperfeiçoamento das minhas tecnicas em programação</p>
+                    <h3>Competências</h3>
+                    <p>JavaScript, HTML, CSS, Node.js, React, Desenvolvimento Web.
+                    </p>
                 </div>
                 <div className='conteudo-single'>
-                    <h3>Titulo</h3>
-                    <p>Isso é somente para questões de estudo e aperfeiçoamento das minhas tecnicas em programação</p>
+                    <h3>Redes Sociais</h3>
+                    <p>LinkedIn: (link), GitHub: (link)</p>
                 </div>
                 <div className='conteudo-single'>
-                    <h3>Titulo</h3>
-                    <p>Isso é somente para questões de estudo e aperfeiçoamento das minhas tecnicas em programação</p>
+                    <h3>Objetivo</h3>
+                    <p>Meu objetivo é todos os dias me aperfeiçoar nesta minha nova jornada, adquirindo conhecimento e os colocando em prática, para que assim possa ser o melhor possível em meu trabalho. </p>
                 </div>
             </div>
 
